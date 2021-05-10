492f90b3fcba7481bcd155672e17d8a1db6c2b81
Aymeric <Aymeric.Daurelle@cdiscount.com>
2021-05-10 14:48:58 +0200

    connect to mysql database

--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"net/http"
 
+	"github.com/aymericdd/golan-webservices/database"
 	"github.com/aymericdd/golan-webservices/product"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 const apiBasePath = "/api"
 
 func main() {
+	database.SetupDatabase()
 	product.SetupRoutes(apiBasePath)
 	http.ListenAndServe(":5000", nil)
 }
